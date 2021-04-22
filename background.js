1e9abb4e2d431e47e43de37f060e5768641ce7a9
Martin M <maroymart@gmail.com>
2021-04-21 22:31:30 -0400

    Added some side cases

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 chrome.storage.sync.set({
 	"target": 0,
+	"interval": 500,
+	"hack_type": "set"
 });
-
 const attach_and_detach = (tabId) => {
 	chrome.tabs.query({
 		url: "https://monkeytype.com/"
@@ -45,19 +46,68 @@ chrome.webNavigation.onCompleted.addListener(({ tabId }) => attach_and_detach(ta
 chrome.tabs.onHighlighted.addListener((tabId, windowId) => attach_and_detach(tabId));
 let timeout = 0;
 let delay = 500;
+let cancel = false;
 
 chrome.runtime.onConnect.addListener(port => {
 	console.assert(port.name == "hack");
 	port.onMessage.addListener(msg => {
 		if(msg.type == "letters") {
+			cancel = timeout ? true : false;
 			clearTimeout(timeout);
 			chrome.storage.sync.get("target", data => {
 				if(data != 0) {
 					let index = 0;
 					const sendKey = () => {
 						timeout = setTimeout(() => {
-							if(index == msg.letters.length) return clearTimeout(timeout);
-							console.log(msg.letters[index]);
+							if(index == msg.letters.length || cancel) {
+								if(cancel) {
+									cancel = false;
+									clearTimeout(timeout);
+									timeout = 0;
+									chrome.debugger.sendCommand({
+										tabId: data.target
+									}, "Input.dispatchKeyEvent", {
+										type: "keyDown",
+										text: '',
+										key: 'Backspace',
+										code: 'Backspace',
+										modifiers: 0
+									}, () => {
+										chrome.debugger.sendCommand({
+											tabId: data.target
+										}, "Input.dispatchKeyEvent", {
+											type: "keyDown",
+											text: '',
+											key: 'Backspace',
+											code: 'Backspace',
+											modifiers: 0
+										}, () => {
+											chrome.debugger.sendCommand({
+												tabId: data.target
+											}, "Input.dispatchKeyEvent", {
+												type: "keyDown",
+												text: '',
+												key: 'Backspace',
+												code: 'Backspace',
+												modifiers: 0
+											}, () => {
+												index--;
+											});
+										});
+									});
+								} else {
+									return clearTimeout(timeout);
+								}
+							};
+							/* if(msg.hack_type == "set") {
+								delay = delay;
+							} else if(msg.hack_type == "natural") {
+								const type = msg.natural_type;
+								switch(type) {
+									case 'noob':
+										delay = 
+								}
+							} */
 							chrome.debugger.sendCommand({
 								tabId: data.target
 							}, "Input.dispatchKeyEvent", {
@@ -68,7 +118,6 @@ chrome.runtime.onConnect.addListener(port => {
 								modifiers: 0
 							}, e => {
 								if(chrome.runtime.lastError) console.log(chrome.runtime.lastError);
-								if(chrome.runtime.lastError.startsWith("Debugger is not attached to the tab")) clearInterval(interval);
 							});
 							index++;
 							sendKey();
--- main.js
+++ main.js
@@ -1,25 +1,13 @@
 window.onload = () => {
-	setTimeout(() => {
-		let letters = Array.from(document.getElementById("words").children).map(word => Array.from(word.children).map(l => l.innerText)).map(w => w.join("")).join(" ").split("");
-		const restartButton = document.getElementById("restartTestButton");
-		console.log(letters[0]);
-		chrome.storage.sync.set({
-			"interval": 500,
-		});
-
-		let port;
-		port = chrome.runtime.connect({name: "hack"});
-		port.postMessage({
-			type: "letters",
-			letters
-		});
-		port.onMessage.addListener(msg => {
-			console.log(msg);
-		});
-
-		restartButton.addEventListener("click", () => {
+	chrome.storage.sync.get("hack_type", data => {
+		if(data.hack_type == "set") {
 			setTimeout(() => {
-				letters = Array.from(document.getElementById("words").children).map(word => Array.from(word.children).map(l => l.innerText)).map(w => w.join("")).join(" ").split("");
+				let letters = Array.from(document.getElementById("words").children).map(word => Array.from(word.children).map(l => l.innerText)).map(w => w.join("")).join(" ").split("");
+				const restartButton = document.getElementById("restartTestButton");
+				console.log(letters[0]);
+		
+				let port;
+				port = chrome.runtime.connect({name: "hack"});
 				port.postMessage({
 					type: "letters",
 					letters
@@ -27,23 +15,37 @@ window.onload = () => {
 				port.onMessage.addListener(msg => {
 					console.log(msg);
 				});
-			}, 1000);
-		});
+		
+				restartButton.addEventListener("click", () => {
+					setTimeout(() => {
+						letters = Array.from(document.getElementById("words").children).map(word => Array.from(word.children).map(l => l.innerText)).map(w => w.join("")).join(" ").split("");
+						if(!port) port = chrome.runtime.connect({name: "hack"});
+						port.postMessage({
+							type: "letters",
+							letters
+						});
+						port.onMessage.addListener(msg => {
+							console.log(msg);
+						});
+					}, 1000);
+				});
 
-		document.getElementById("words").addEventListener("click", e => {
-			console.log("words");
-			port.postMessage({
-				type: "stop"
-			});
-		});
-		/* chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-			chrome.debugger.sendCommand({
-				tabId: sender.tab.id
-			}, "Input.dispatchKeyEvent", {
-				type: "keyDown",
-				text: letters[0],
-				unmodifiedText: letters[0],
-			});
-		}); */
-	}, 1000);
+				document.getElementById("words").addEventListener("click", e => {
+					console.log("words");
+					port.postMessage({
+						type: "stop"
+					});
+				});
+				/* chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+					chrome.debugger.sendCommand({
+						tabId: sender.tab.id
+					}, "Input.dispatchKeyEvent", {
+						type: "keyDown",
+						text: letters[0],
+						unmodifiedText: letters[0],
+					});
+				}); */
+			}, 1000);
+		}
+	});
 }
